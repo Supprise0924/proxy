1af8d6617775205b2feca735674d800de5c0d19a
Alan Bobs <amna25499@gmail.com>
2022-10-08 23:27:32 +0800

    feat: add configparser for uniting various apps

---
+++ utils/configparser.py
@@ -0,0 +1,7 @@
+import configparser
+
+def configparser():
+    external_config = {'litespeedtest': './speedtest/config.json', 'subconverter': './subconverter/generate.ini'}
+    config = './config.ini'
+    
+    
