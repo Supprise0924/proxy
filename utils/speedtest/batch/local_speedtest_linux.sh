c690104ac7bbf7c830e79c5796aa5791c136dcfe
Alan Bobs <amna25499@gmail.com>
2022-10-09 14:36:41 +0800

    refactor: add command line support for speedtest

--- utils/speedtest/batch/local_speedtest_linux.sh
+++
@@ -1,8 +0,0 @@
-#准备好所需文件
-test -e lite && echo "Listespeedtest is ready to run." || { echo "LiteSpeedTest is not exist, downloading from GitHub release."; wget -O lite.gz https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.11.2m/lite-linux-amd64-v0.11.2m.gz; gunzip lite.gz;}
-#运行 LiteSpeedTest 开始测速
-chmod +x ./lite
-sudo nohup ./lite --config ../lite_config.json --test https://raw.githubusercontent.com/alanbobs999/TopFreeProxies/master/Eternity.yml >speedtest.log 2>&1 &
-#导出筛选结果(仓库根目录的两个 Eternity 文件)
-python ../output.py
-python ../../main.py
--- utils/speedtest/batch/local_speedtest_win.bat
+++
@@ -1,9 +0,0 @@
-if exist ./lite.exe (
-    echo "LiteSpeedTest is ready to run."
-) else (
-    echo "LiteSpeedTest is not exist, please download from https://github.com/alanbobs999/LiteSpeedTest/releases/download/v0.11.2m/lite-windows-amd64-v0.11.2m.zip. Move .exe file to root and rename it to lite.exe."
-)
-
-lite.exe --config ../lite_config.json --test https://raw.githubusercontent.com/alanbobs999/TopFreeProxies/master/Eternity
-python ../output.py
-python ../../main.py
