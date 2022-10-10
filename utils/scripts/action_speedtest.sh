5e97884328d4fef0aa346e5ac1594ffe5f071fc7
Alan Bobs <amna25499@gmail.com>
2022-10-10 16:57:57 +0800

    chore: update action scripts

--- utils/scripts/action_speedtest.sh
+++ utils/scripts/action_speedtest.sh
@@ -11,4 +11,5 @@ mv -f ./utils/scripts/proxychains.conf ../../../../../../etc/proxychains.conf
 sudo pkill -f clash
 ./clash -f ./utils/shell/clash_config.yml &
 # Begin merge
+chmod +x ./utils/subconverter/subconverter && chmod +x ./utils/litespeedtest/lite
 sudo nohup proxychains python3 ./utils/main.py >merge.log 2>&1 &
