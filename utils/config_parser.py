251240f7f81db01388296fd4e05cfce67a69d455
Alan Bobs <amna25499@gmail.com>
2022-10-12 22:16:10 +0800

    chore: provider more info

--- utils/config_parser.py
+++ utils/config_parser.py
@@ -17,6 +17,7 @@ def configparse(section):
         return config['speedtest']
 
 def externalhandler():
+    # Litespeedtest config handler
     with open(external_config['speedtest'], 'r', encoding='utf-8') as f:
         lite_config = json.load(f)
         for key in config['speedtest']:
@@ -26,3 +27,7 @@ def externalhandler():
                 lite_config[key] = config['speedtest'].get(key)
     with open(external_config['speedtest'], 'w', encoding='utf-8') as f:
         f.write(json.dumps(lite_config, sort_keys=False, indent=4, ensure_ascii=False))
+
+    # Subconverter config handler
+    # https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6
+    
