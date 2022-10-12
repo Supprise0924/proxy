bf11ab7689b15dc2dcd10b8ee903761ab38c8631
Alan Bobs <amna25499@gmail.com>
2022-10-12 23:32:21 +0800

    chore: integrate config_parser into main

--- utils/main.py
+++ utils/main.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
 
 import os, urllib
+import configparser, json
 
 from sub_merge import merge
 from sub_update import update
-from config_parser import configparse, externalhandler
+
+
+config_file = './utils/config.ini'
+external_config = {'speedtest': './utils/litespeedtest/config.json', 'subconverter': './utils/subconverter/generate.ini'}
+
+
+def configparse(section):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    if section == 'common':
+        return config['common']
+    elif section == 'subconverter':
+        return config['subconverter']
+    elif section == 'speedtest':
+        return config['speedtest']
+
+def externalconfighandler():
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    # Litespeedtest config handler
+    with open(external_config['speedtest'], 'r', encoding='utf-8') as f:
+        lite_config = json.load(f)
+        for key in config['speedtest']:
+            if isinstance(lite_config[key],int):
+                lite_config[key] = config['speedtest'].getint(key)
+            else:
+                lite_config[key] = config['speedtest'].get(key)
+    with open(external_config['speedtest'], 'w', encoding='utf-8') as f:
+        f.write(json.dumps(lite_config, sort_keys=False, indent=4, ensure_ascii=False))
+
+    # Subconverter config handler
+    # https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6
+    
 
 if __name__ == '__main__':
-    externalhandler() # Initialize config
+    externalconfighandler() # Initialize config
 
     print('Downloading Country.mmdb...')
     try:
